a10e19796d21db6e16af3792c656124fdbd84bf8
christelle flores <57645973+christelleflores@users.noreply.github.com>
2021-03-15 23:29:25 -0400

    asks user for num of credits, and checks if num of credits is valid, if not asks user again. makes credit variable and updates every time user inputs a new class with credits. prints total credits.

--- src/gpa.java
+++ src/gpa.java
@@ -19,47 +19,65 @@ public class gpa {
 			System.out.println("How many classes are you taking?");
 			int numClasses = scan.nextInt();
 			scan.nextLine();
-			
+
 			int count = 0;
-			
-			while(count < numClasses)
-			{
+			int credits = 0;
+
+			while (count < numClasses) {
 				System.out.println("What is the name of class " + (count + 1));
 				String name = scan.nextLine();
+				System.out.println("How many credits is the class?");
+				int cred = scan.nextInt();
+				while (cred != 1 && cred != 3) {
+					System.out.println("That is not a valid credit amount.");
+					System.out.println("How many credits is the class?");
+					cred = scan.nextInt();
+				}
+				scan.nextLine();
+				credits = credits + cred;
 				double classValue = classValue();
 				out.println(name + ": " + classValue);
 				count++;
 			}
-			
+			out.println("Total Credits: " + credits);
 			out.close();
 			fileWriter.close();
-			
-			} catch (IOException e) {
+
+		} catch (IOException e) {
 			e.printStackTrace();
-			}	
+		}
 		System.out.println("Your file has been stored.");
-	}	
-		
-	public static double classValue()
-	{
+	}
+
+	public static double classValue() {
 		Scanner console = new Scanner(System.in);
-	
+
 		System.out.println("Please enter your letter grade, for instance, A, A-, B, C-, etc. for that course");
 		String grade = console.nextLine();
-		
-		if (grade.equals("A")) return 4.0;
-		else if(grade.equals("A-")) return 3.6;
-		else if(grade.equals("B+")) return 3.2;
-		else if(grade.equals("B")) return 3.0;
-		else if(grade.equals("B-")) return 2.6;
-		else if(grade.equals("C+")) return 2.2;
-		else if(grade.equals("C")) return 2.0;
-		else if(grade.equals("C-")) return 1.6;
-		else if(grade.equals("D")) return 1.2;
-		else if(grade.equals("D-")) return 1.0;
-		else if(grade.equals("F")) return 0.0;
-		else
-		{
+
+		if (grade.equals("A"))
+			return 4.0;
+		else if (grade.equals("A-"))
+			return 3.6;
+		else if (grade.equals("B+"))
+			return 3.2;
+		else if (grade.equals("B"))
+			return 3.0;
+		else if (grade.equals("B-"))
+			return 2.6;
+		else if (grade.equals("C+"))
+			return 2.2;
+		else if (grade.equals("C"))
+			return 2.0;
+		else if (grade.equals("C-"))
+			return 1.6;
+		else if (grade.equals("D"))
+			return 1.2;
+		else if (grade.equals("D-"))
+			return 1.0;
+		else if (grade.equals("F"))
+			return 0.0;
+		else {
 			System.out.println("Sorry, this is invalid. Please enter a valid letter grade.");
 			return classValue();
 		}
