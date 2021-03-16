1c226af82c8d2118abe1240506264c5970970e99
sahalk903 <sydneyhalk@gmail.com>
2021-03-15 23:09:30 -0400

    deleted variables that will be implemented later in try catch statement that was just created

--- src/gpa.java
+++ src/gpa.java
@@ -1,41 +1,31 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class gpa {
 	public static void main(String args[]) {
-
-		String grades = "";
-		String[] gradesletter = { "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "D-", "F" };
-		double[] gradesDecimal = { 4.0, 3.6, 3.2, 3.0, 2.6, 2.2, 2.0, 1.6, 1.2, 1.0, 0.0 };
-		double gradeAmount = 0;
-		double totalAmountCourse1 = 0;
-		double totalAmountCourse2 = 0;
-		double totalAmountCourse3 = 0;
-		double totalAmountCourse4 = 0;
-		double totalAmountCourse5 = 0;
-		double totalAmountCourse6 = 0;
-		double FinalAmount = 0;
-		double FinalCredit = 0;
-		double gpa;
-
-		System.out.println("Hello students of Quinnipiac University, would you like to calculate your GPA?");
-		String message;
-
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter your first name");
-		message = scan.nextLine();
-		System.out.println("Please enter your last name");
-		message = scan.nextLine();
-		System.out.println("Please enter your major ");
-		message = scan.nextLine();
-		System.out.println("Please enter your QU ID number, located on your QU Card ");
-		message = scan.nextLine();
-		System.out.println("Please enter the CourseID for a class you are currently taking ");
-		message = scan.nextLine();
-
-		// Use the method below in a loop to account for any number of classes
+		try {
+			FileWriter fileWriter = new FileWriter("gpa.txt");
+			PrintWriter out = new PrintWriter(fileWriter);
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Please enter your full name");
+			out.println("Name: " + scan.nextLine());
+			System.out.println("Please enter your major ");
+			out.println("Major: " + scan.nextLine());
+			System.out.println("Please enter your QU ID number, located on your QU Card ");
+			out.println("QU ID: " + scan.nextInt());
+			scan.nextLine();
+			System.out.println("How many classes are you taking?");
+			int numClasses = scan.nextInt();
+			out.close();
+			fileWriter.close();
+			} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		System.out.println("Your file has been stored.");
+	}	
 		
-	}
-	
 	public static double classValue()
 	{
 		Scanner console = new Scanner(System.in);
